773c073ecec21c1a1ccd2832b8225e0415193005
nkrch <111977977+nkrch@users.noreply.github.com>
2023-11-12 14:54:41 +0300

    Update simply.js

--- simply.js
+++ simply.js
@@ -1,3 +1,10 @@
-for(i=1; i<=1000; i++){
-    console.log(i);
+let isProst;
+for(i=2; i<=1000; i++){
+    isProst=true;
+    for (j=2;j<i;j++){
+        if (i%j==0){
+            isProst=false;
+        }
+    }
+    if(isProst)console.log(i);
 }
